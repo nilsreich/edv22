cd94df1631eb1032271a8defd601f37c4a015230
nilsreich <nilsreich@gmail.com>
2022-06-28 09:00:07 +0000

    SupabaseIssue Title Fixes #1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
-import { Sidebar } from './../components/Sidebar';
-import type { NextPage } from 'next'
-import { useState } from 'react'
-import useSWR from 'swr'
+import { Sidebar } from "./../components/Sidebar";
+import type { NextPage } from "next";
+import { useState, useEffect } from "react";
+import { supabase } from "../utils/supabaseClient";
+import Auth from "../components/Auth";
+import Account from "../components/Account";
+import useSWR from "swr";
 import * as DropdownMenuPrimitive from "@radix-ui/react-dropdown-menu";
 
 const fetcher = async (
@@ -14,14 +17,21 @@ const fetcher = async (
 };
 
 const Home: NextPage = () => {
+  const [session, setSession] = useState<any>(null);
 
-  const [active, setActive] = useState('Din5008')
-  const { data, error } = useSWR('./content.json', fetcher)
+  useEffect(() => {
+    setSession(supabase.auth.session());
 
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+  const [active, setActive] = useState("Din5008");
+  const { data, error } = useSWR("./content.json", fetcher);
 
   const changeView = (item: any) => {
-    setActive(item.target.innerText)
-  }
+    setActive(item.target.innerText);
+  };
 
   const Dropdown = () => (
     <DropdownMenuPrimitive.Root>
@@ -32,31 +42,36 @@ const Home: NextPage = () => {
         <DropdownMenuPrimitive.Item>Item</DropdownMenuPrimitive.Item>
       </DropdownMenuPrimitive.Content>
     </DropdownMenuPrimitive.Root>
-  )
-
+  );
 
   return (
     <>
-      {data ?
-
-        <div className='flex'>
-          <aside className='border-r w-40 shrink-0 h-screen '>
+      {data ? (
+        <div className="flex">
+          <aside className="border-r w-40 shrink-0 h-screen ">
             <Sidebar parentcallback={changeView} data={data} active={active} />
           </aside>
-          <main className='mx-auto max-w-4xl p-12'>
-            <div className='text-6xl'>{data[active].title}</div>
+          <main className="mx-auto max-w-4xl p-12">
+            <div className="text-6xl">{data[active].title}</div>
             {data[active].situation}
             <div>
               <video controls src={`.\\${data[active].svg}`}></video>
             </div>
             <Dropdown />
+            <div className="container" style={{ padding: "50px 0 100px 0" }}>
+              {!session ? (
+                <Auth />
+              ) : (
+                <Account key={session.user.id} session={session} />
+              )}
+            </div>
           </main>
         </div>
-        : ''}
+      ) : (
+        ""
+      )}
     </>
-  )
-}
+  );
+};
 
-export default Home
-
-
+export default Home;
