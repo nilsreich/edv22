a88ddb19bf0d46bc946c1989a4c0aa8043144cc1
nilsreich <nilsreich@gmail.com>
2022-06-28 22:19:55 +0000

    finish

--- stories/Button.tsx
+++ stories/Button.tsx
@@ -1,40 +1,18 @@
 import React from "react";
 
 interface ButtonProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
   primary?: boolean;
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
   size?: "small" | "medium" | "large";
-  /**
-   * Button contents
-   */
   label: string;
-  /**
-   * Optional click handler
-   */
-  onClick?: () => void;
 }
 
-/**
- * Primary UI component for user interaction
- */
 export const Button = ({
   primary = false,
   size = "medium",
   label,
   ...props
 }: ButtonProps) => {
-  const mode = primary
-    ? "bg-blue-400"
-    : "";
+  const mode = primary ? "bg-blue-400" : "";
 
   const sizeClass = () => {
     switch (size) {
@@ -45,16 +23,11 @@ export const Button = ({
       default:
         return "";
     }
-  }
-  
+  };
+
   return (
-    <button
-      className={`storybook-button ${mode} ${sizeClass()}`}
-      {...props}
-    >
+    <button className={` ${mode} ${sizeClass()} border rounded`} {...props}>
       {label}
     </button>
-
-
   );
 };
