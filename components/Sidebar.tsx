519738bba89f0edd562397130db876c1c9545897
nilsreich <nilsreich@gmail.com>
2022-06-28 09:11:05 +0000

    context

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,13 +1,25 @@
+import { useContext } from "react";
+import { LevelContext } from "../utils/LevelContext.js";
 
-export const Sidebar = ({
-  parentcallback, data, active
-}: any) => {
-  return <>
-  <div className="px-4 text-sm font-semibold pt-4">Stationen</div>
-  <ul>
-    {Object.keys(data).map((item, key) => <li className={` px-4 py-1 ${(active === item) ? 'text-blue-500 font-medium' : ''}`} key={item} onClick={parentcallback}>
-      {item}
-    </li>)}
-  </ul>
-  </>;
+export const Sidebar = ({ parentcallback, data, active }: any) => {
+  const level = useContext(LevelContext);
+
+  return (
+    <>
+      <div className="px-4 text-sm font-semibold pt-4">Stationen - {level}</div>
+      <ul>
+        {Object.keys(data).map((item, key) => (
+          <li
+            className={` px-4 py-1 ${
+              active === item ? "text-blue-500 font-medium" : ""
+            }`}
+            key={item}
+            onClick={parentcallback}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,18 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Link from "next/link";
+import { useContext } from "react";
+import { LevelContext } from "../utils/LevelContext.js";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const level = useContext(LevelContext);
   return (
-    <>
+    <LevelContext.Provider value={3}>
       {" "}
       <ul>
         <li>
           <Link href="/">
-            <a>Home</a>
+            <a>Home - {level}</a>
           </Link>
         </li>
         <li>
@@ -19,7 +22,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         </li>
       </ul>
       <Component {...pageProps} />
-    </>
+    </LevelContext.Provider>
   );
 }
 
